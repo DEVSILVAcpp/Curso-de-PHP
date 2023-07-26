626922048cb28987042b18f6633124a99796f9c4
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-07-26 09:50:59 -0300

    CURSO DE PHP 7 - FUNDAMENTOS BÁSICOS

---
+++ Aula05.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fundamentos Básicos</title>
+</head>
+<body>
+	
+<?php 
+// esse é meu primeiro arquivo php
+/* meu primeiro arquivo */
+echo "Meu nome é Josias <br>";
+print "Minha idade é 20";
+?>
+
+</body>
+</html>
