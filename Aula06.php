a4e973f22f7b69b74e91186784a23515b67d80d2
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-07-26 10:43:43 -0300

    CURSO DE PHP 7 - VARIÁVEIS

---
+++ Aula06.php
@@ -0,0 +1,38 @@
+<?php
+// Meus Dados
+$nome = "Josias Gabriel da Silva";
+$idade = 23;
+$altura = 1.83;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+// Alterando meus dados
+$nome = "Felipe Cabral";
+$idade = 29;
+$altura = 1.90;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+echo "<hr>";
+
+$carro = "Hilux";
+$Carro = "Fusca";
+$meunome = "Josias";
+
+echo $Carro;
+echo "<br>";
+echo $meunome;
+
+/* Nomes Válidos
+$nome
+$_meunome
+$carro10
+$nota1
+$meu_nome
+$meuNome
+*/ 
+
+/* Nomes Inválidos 
+$123456nome
+$carro*&!@#
+$meu carro
+*/
