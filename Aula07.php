2862181695de10ebaab03f8959f1720308555de2
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-07-27 08:36:37 -0300

    CURSO DE PHP 7 - VARIÁVEIS DINÂMICAS

---
+++ Aula07.php
@@ -0,0 +1,17 @@
+<?php
+// Variáveis variáveis
+
+$bebida = "refrigeranre";
+
+$$bebida = "Guaraná";
+
+echo $refrigeranre;
+echo "<br>";
+
+////////////////////////////////////////
+
+$destino = "cidade";
+
+$$destino = "Ilheus";
+
+echo $cidade;
