1611ee2e8f29960834abac4f98d2c67a1188a9d3
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-07-29 14:34:02 -0300

    CURSO DE PHP 7 - ARRAYS #2

---
+++ Aula13.php
@@ -0,0 +1,28 @@
+<?php
+//Arrays
+$carros = array(1=>"BMW",2=>"Veloster",3=>"Hilux");
+$carros[] = "Amarok";
+$carros[10] = "Camaro";
+echo $carros[10];
+echo "<br>";
+
+$motos = array();
+$motos[] = "Yamara";
+$motos[] = "Honda";
+$motos[5] = "Suzuki";
+echo $motos[5];
+echo "<br>";
+
+$clientes = ["Rodrigo", "Felipe", "bia"];
+print_r($clientes);
+echo "<hr>";
+
+//Count
+$totalClientes = count($clientes);
+echo $ctotalClientes;
+echo "<hr>";
+
+//Foreach
+foreach ($carros as $valor) {
+	echo $valor."<br>";
+}
